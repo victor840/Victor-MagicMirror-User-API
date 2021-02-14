6c605a2d92f8e24c4810a861f30686115a237fc1
victor840 <victor.ugoanusi@gmail.com>
2021-02-14 07:32:06 -0500

    first commit

---
+++ src/main/java/MagicMirrorAPI/UserGoals/UserGoalsApplication.java
@@ -0,0 +1,13 @@
+package MagicMirrorAPI.UserGoals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserGoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserGoalsApplication.class, args);
+	}
+
+}
